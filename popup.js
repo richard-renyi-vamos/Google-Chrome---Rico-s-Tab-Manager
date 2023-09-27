c80e35cfec4e78b954a8761256ba166787b6d09b
Richard Renyi-Vamos <82152266+richard-renyi-vamos@users.noreply.github.com>
2023-09-27 14:07:08 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,9 +2,20 @@
 chrome.tabs.query({}, function (tabs) {
   var tabList = document.getElementById("tabList");
 
-  tabs.forEach(function (tab) {
+  tabs.forEach(function (tab, index) {
     var listItem = document.createElement("li");
-    listItem.textContent = tab.title;
+    var link = document.createElement("a");
+    link.textContent = (index + 1) + ". " + tab.title;
+    link.href = "#";
+
+    // Add a click event listener to focus on the tab and its window when clicked
+    link.addEventListener("click", function () {
+      chrome.tabs.update(tab.id, { active: true }, function(updatedTab) {
+        chrome.windows.update(updatedTab.windowId, { focused: true });
+      });
+    });
+
+    listItem.appendChild(link);
     tabList.appendChild(listItem);
   });
 });
